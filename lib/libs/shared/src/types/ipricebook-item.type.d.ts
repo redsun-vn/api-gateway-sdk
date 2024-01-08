cc8eabb7c16c427a2c6a45d87ae1e0f24d153779
elmiscor <nguyenleminh.it@gmail.com>
2024-01-08 14:13:59 +0700

    refactor(pricebook-item): updated types

--- lib/libs/shared/src/types/ipricebook-item.type.d.ts
+++ lib/libs/shared/src/types/ipricebook-item.type.d.ts
@@ -2,25 +2,16 @@ export declare namespace IPricebookItem {
     interface IPricebookItemResponse {
         shop_id: string | number;
         price_book_id: string | number;
-        price_book_rule_id: string | number;
         variant_id: string | number;
-        referent_id?: string | number | null;
         variantPrice: number | string;
         variantOriginPrice: number | string;
         salePrice: number | string | null;
         priceBookName: string | null;
-        foundAt: string | null;
-        foundDate: string | null;
-        priceBookEffectiveAt: string | null;
-        priceBookExpiredAt: string | null;
-        referentType: string | null;
+        foundAtWithoutTz: string | null;
         ruleReferentValue: string | null;
+        ruleDaysOfTheWeek: string | null;
         ruleEffectiveHour: string | null;
-        ruleRecipeFee: string | number;
-        ruleType: string | null;
-        ruleStayOn: string | null;
         ruleExpiredAt: string | null;
         ruleEffectiveAt: string | null;
-        ruleValue: string | number;
     }
 }
