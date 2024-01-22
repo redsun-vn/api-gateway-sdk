f65f476bbeafa2e29f8c2435fb41bf960de6828a
elmiscor <nguyenleminh.it@gmail.com>
2024-01-22 10:29:42 +0700

    feat(pricebook): add fields

--- lib/libs/shared/src/types/product-service/ipricebook.d.ts
+++ lib/libs/shared/src/types/product-service/ipricebook.d.ts
@@ -2,9 +2,13 @@ import { tags } from 'typia';
 import { BaseResponse } from '../common.type';
 import { IPricebookItem } from './ipricebook-item.type';
 import { IPricebookRule } from './ipricebook-rule.type';
+import { IProduct } from './iproduct';
 export declare namespace IPricebook {
     interface IReqCreatePricebook {
         name: string;
+        branch_id?: number & tags.Type<'uint32'>;
+        department_id?: number & tags.Type<'uint32'>;
+        salesChannel?: IProduct.ISalesChannel[];
         effectiveAt?: string & tags.Format<'date-time'>;
         expiredAt?: string & tags.Format<'date-time'>;
         active?: boolean;
@@ -15,6 +19,9 @@ export declare namespace IPricebook {
     }
     interface IUpdatePricebook {
         id?: string;
+        branch_id?: number & tags.Type<'uint32'>;
+        department_id?: number & tags.Type<'uint32'>;
+        salesChannel?: IProduct.ISalesChannel[];
         name?: string;
         effectiveAt?: string & tags.Format<'date-time'>;
         expiredAt?: string & tags.Format<'date-time'>;
@@ -23,6 +30,9 @@ export declare namespace IPricebook {
     }
     interface IPricebookResponse extends BaseResponse {
         shop_id: string | number | null;
+        branch_id?: string | number | null;
+        department_id?: string | number | null;
+        salesChannel?: IProduct.ISalesChannel[] | null;
         name: string;
         active: boolean;
         effectiveAt?: string | null;
