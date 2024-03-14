7e49f60320883138220887b9fe9076f85a8ac613
nam-redsun <namvu@redsun.vn>
2024-03-14 14:29:40 +0700

    feat(shop): remove shop data from api find by name

--- lib/api/functional/shops/find_by_name/index.d.ts
+++ lib/api/functional/shops/find_by_name/index.d.ts
@@ -1,9 +1,8 @@
 import type { IConnection, Primitive } from "@nestia/fetcher";
 import type { IResponse } from "../../../../libs/shared/src/types/common.type";
-import type { IShop } from "../../../../libs/shared/src/types/ishop";
 export declare function findOneByName(connection: IConnection, name: string): Promise<findOneByName.Output>;
 export declare namespace findOneByName {
-    type Output = Primitive<IResponse<IShop.IShopResponse>>;
+    type Output = Primitive<IResponse<false | true>>;
     const METADATA: {
         readonly method: "GET";
         readonly path: "/shops/find-by-name/:name";
