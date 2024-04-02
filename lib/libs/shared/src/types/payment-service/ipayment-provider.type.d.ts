6837b8eca06531c6fbb199f83c7bdd4e87d79c09
Lan Vu <ibmlenovo1996@gmail.com>
2024-04-02 13:43:02 +0700

    feat(vietqr): update dto create viet qr

--- lib/libs/shared/src/types/payment-service/ipayment-provider.type.d.ts
+++ lib/libs/shared/src/types/payment-service/ipayment-provider.type.d.ts
@@ -1,7 +1,7 @@
 import { tags } from 'typia';
 export declare namespace IPaymentProvider {
     interface IVietQRCreate {
-        transactionId: number & tags.Type<'uint64'>;
+        paymentId: number & tags.Type<'uint64'>;
         bankId: string;
         addInfo: string;
         accountName: string;
