fafbac45635208ae01b7f67862544fb47d225d20
nam-redsun <namvu@redsun.vn>
2024-04-09 13:31:57 +0700

    update

--- lib/api/functional/shop/subscriptions/stats/index.d.ts
+++ lib/api/functional/shop/subscriptions/stats/index.d.ts
@@ -1,12 +1,12 @@
 import type { IConnection, Primitive } from "@nestia/fetcher";
 import type { IResponse } from "../../../../../libs/shared/src/types/common.type";
 import type { ISubscription } from "../../../../../libs/shared/src/types/isubscription";
-export declare function checkStats(connection: IConnection): Promise<checkStats.Output>;
+export declare function checkStats(connection: IConnection, id: string): Promise<checkStats.Output>;
 export declare namespace checkStats {
     type Output = Primitive<IResponse<ISubscription.IStats>>;
     const METADATA: {
         readonly method: "GET";
-        readonly path: "/shop/subscriptions/stats";
+        readonly path: "/shop/subscriptions/:id/stats";
         readonly request: null;
         readonly response: {
             readonly type: "application/json";
@@ -14,5 +14,5 @@ export declare namespace checkStats {
         };
         readonly status: null;
     };
-    const path: () => string;
+    const path: (id: string) => string;
 }
