05a233f8d9cf839c751a0536d00d4664d87d1a67
Lan Vu <ibmlenovo1996@gmail.com>
2024-09-26 11:22:29 +0700

    feat(partner-report): get detail partner report

--- lib/api/functional/shop/report/index.d.ts
+++ lib/api/functional/shop/report/index.d.ts
@@ -4,3 +4,4 @@ export * as staff_selling from "./staff_selling";
 export * as order_revenues from "./order_revenues";
 export * as order_profits from "./order_profits";
 export * as order_revenue from "./order_revenue";
+export * as partner_report from "./partner_report";
---
+++ lib/api/functional/shop/report/partner_report/index.d.ts
@@ -0,0 +1,35 @@
+import type { IConnection, Resolved, Primitive } from "@nestia/fetcher";
+import type { IQuery, IResponse, IResponsePagination } from "../../../../../libs/shared/src/types/common.type";
+import type { IPartnerReport } from "../../../../../libs/shared/src/types/report-service/ipartner-report.type";
+export declare function getPartnerReport(connection: IConnection, query: getPartnerReport.Query): Promise<getPartnerReport.Output>;
+export declare namespace getPartnerReport {
+    type Query = Resolved<IQuery>;
+    type Output = Primitive<IResponse<IResponsePagination<IPartnerReport.IPartnerReportResponse>>>;
+    const METADATA: {
+        readonly method: "GET";
+        readonly path: "/shop/report/partner-report";
+        readonly request: null;
+        readonly response: {
+            readonly type: "application/json";
+            readonly encrypted: false;
+        };
+        readonly status: null;
+    };
+    const path: (query: getPartnerReport.Query) => string;
+}
+export declare function getPartnerDetailReport(connection: IConnection, query: getPartnerDetailReport.Query, partnerId: string): Promise<getPartnerDetailReport.Output>;
+export declare namespace getPartnerDetailReport {
+    type Query = Resolved<IQuery>;
+    type Output = Primitive<IResponse<IPartnerReport.IPartnerReportResponse>>;
+    const METADATA: {
+        readonly method: "GET";
+        readonly path: "/shop/report/partner-report/:partner_id";
+        readonly request: null;
+        readonly response: {
+            readonly type: "application/json";
+            readonly encrypted: false;
+        };
+        readonly status: null;
+    };
+    const path: (query: getPartnerDetailReport.Query, partnerId: string) => string;
+}
