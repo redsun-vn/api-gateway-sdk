0cd1d8595bfa738de917b99939cc6671306c910e
Lan Vu <ibmlenovo1996@gmail.com>
2024-10-30 17:20:45 +0700

    fix(sdk): update sdk

--- lib/api/functional/shop/report/index.d.ts
+++ lib/api/functional/shop/report/index.d.ts
@@ -1,4 +1,5 @@
 export * as payment_cards from "./payment_cards";
+export * as attendance_overview from "./attendance_overview";
 export * as best_selling from "./best_selling";
 export * as staff_selling from "./staff_selling";
 export * as order_revenues from "./order_revenues";
--- lib/libs/shared/src/types/report-service/index.d.ts
+++ lib/libs/shared/src/types/report-service/index.d.ts
@@ -2,3 +2,4 @@ export * from './iorder-revenue.type';
 export * from './ischedule.type';
 export * from './iorder-items.type';
 export * from './ipartner-report.type';
+export * from './iattendance-report.type';
