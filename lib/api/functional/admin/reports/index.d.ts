95af2c6f284c344b265a502aa5e2fb6955c37be9
thanhdai0411 <ngothanhdai0411@gmail.com>
2024-11-07 09:04:31 +0700

    feat(dto): dto

--- lib/api/functional/admin/reports/index.d.ts
+++ lib/api/functional/admin/reports/index.d.ts
@@ -1,3 +1,2 @@
 export * as shops from "./shops";
 export * as subscriptions from "./subscriptions";
-export * as shop_inactive from "./shop_inactive";
--- lib/libs/shared/src/types/report-service/index.d.ts
+++ lib/libs/shared/src/types/report-service/index.d.ts
@@ -3,6 +3,3 @@ export * from './ischedule.type';
 export * from './iorder-items.type';
 export * from './ipartner-report.type';
 export * from './iattendance-report.type';
-export interface IReportShopInActive {
-    [key: number]: number[];
-}
