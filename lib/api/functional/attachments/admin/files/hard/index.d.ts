2d37e36b28b90dbdee08e8f326b71931b16933bd
thanhdai0411 <ngothanhdai0411@gmail.com>
2024-12-09 15:22:45 +0700

    feat(api): add api

---
+++ lib/api/functional/attachments/admin/files/hard/index.d.ts
@@ -0,0 +1,17 @@
+import type { IConnection, Primitive } from "@nestia/fetcher";
+import type { IResponse } from "../../../../../../libs/shared/src/types/common.type";
+export declare function hardDelete(connection: IConnection, id: string): Promise<hardDelete.Output>;
+export declare namespace hardDelete {
+    type Output = Primitive<IResponse<false | true>>;
+    const METADATA: {
+        readonly method: "DELETE";
+        readonly path: "/attachments/admin/files/:id/hard";
+        readonly request: null;
+        readonly response: {
+            readonly type: "application/json";
+            readonly encrypted: false;
+        };
+        readonly status: 200;
+    };
+    const path: (id: string) => string;
+}
--- lib/api/functional/attachments/admin/files/index.d.ts
+++ lib/api/functional/attachments/admin/files/index.d.ts
@@ -1,6 +1,7 @@
 import type { IConnection, Resolved, Primitive } from "@nestia/fetcher";
 import type { IResponse } from "../../../../../libs/shared/src/types/common.type";
 import type { IFile } from "../../../../../libs/shared/src/types/ifile";
+export * as hard from "./hard";
 export declare function upload(connection: IConnection, input: upload.Input): Promise<upload.Output>;
 export declare namespace upload {
     type Input = Resolved<IFile.IAdminReqUpload>;
@@ -19,4 +20,34 @@ export declare namespace upload {
         readonly status: 201;
     };
     const path: () => string;
+}
+export declare function findOne(connection: IConnection, id: string): Promise<findOne.Output>;
+export declare namespace findOne {
+    type Output = Primitive<IResponse<IFile.IResponse>>;
+    const METADATA: {
+        readonly method: "GET";
+        readonly path: "/attachments/admin/files/:id";
+        readonly request: null;
+        readonly response: {
+            readonly type: "application/json";
+            readonly encrypted: false;
+        };
+        readonly status: 200;
+    };
+    const path: (id: string) => string;
+}
+export declare function $delete(connection: IConnection, id: string): Promise<$delete.Output>;
+export declare namespace $delete {
+    type Output = Primitive<IResponse<false | true>>;
+    const METADATA: {
+        readonly method: "DELETE";
+        readonly path: "/attachments/admin/files/:id";
+        readonly request: null;
+        readonly response: {
+            readonly type: "application/json";
+            readonly encrypted: false;
+        };
+        readonly status: 200;
+    };
+    const path: (id: string) => string;
 }
