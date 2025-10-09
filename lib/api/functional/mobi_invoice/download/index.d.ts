1242dacd5a824dc7f989ba0bf871d064b17fdc22
avlunvu <ibmlenovo1996@gmail.com>
2025-10-09 15:36:27 +0700

    feat(sdk): update

--- lib/api/functional/mobi_invoice/download/index.d.ts
+++ lib/api/functional/mobi_invoice/download/index.d.ts
@@ -1,10 +1,8 @@
-import type { IConnection, Resolved, Primitive } from "@nestia/fetcher";
-import type { IResponse } from "../../../../libs/shared/src/types/common.type";
+import type { IConnection, Resolved } from "@nestia/fetcher";
 import type { IMobiInvoice } from "../../../../libs/shared/src/types/order-service/imobi-invoice.type";
-export declare function downloadInvoicePDF(connection: IConnection, orderId: string, params: downloadInvoicePDF.Query): Promise<downloadInvoicePDF.Output>;
+export declare function downloadInvoicePDF(connection: IConnection, orderId: string, params: downloadInvoicePDF.Query): Promise<void>;
 export declare namespace downloadInvoicePDF {
     type Query = Resolved<IMobiInvoice.IInvoicePrintParams>;
-    type Output = Primitive<IResponse<any>>;
     const METADATA: {
         readonly method: "GET";
         readonly path: "/mobi-invoice/:orderId/download";
