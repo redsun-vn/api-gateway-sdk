17c34903eed729f63263869acb98734ba9b7f840
avlunvu <ibmlenovo1996@gmail.com>
2025-10-20 11:31:13 +0700

    feat(sdk): update

---
+++ lib/api/functional/e_invoice/download/index.d.ts
@@ -0,0 +1,19 @@
+import type { IConnection, Resolved, Primitive } from "@nestia/fetcher";
+import type { IResponse } from "../../../../libs/shared/src/types/common.type";
+import type { IMisaInvoice } from "../../../../libs/shared/src/types/order-service/imisa-invoice.type";
+export declare function downloadInvoice(connection: IConnection, orderId: string, params: downloadInvoice.Query): Promise<downloadInvoice.Output>;
+export declare namespace downloadInvoice {
+    type Query = Resolved<IMisaInvoice.IInvoicePrintParams>;
+    type Output = Primitive<IResponse<any>>;
+    const METADATA: {
+        readonly method: "GET";
+        readonly path: "/e-invoice/:orderId/download";
+        readonly request: null;
+        readonly response: {
+            readonly type: "application/json";
+            readonly encrypted: false;
+        };
+        readonly status: 200;
+    };
+    const path: (orderId: string, params: downloadInvoice.Query) => string;
+}
