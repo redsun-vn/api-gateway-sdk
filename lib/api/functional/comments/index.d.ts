252dc820dcc1d421af838899a154fcf3dfaefac1
avlunvu <ibmlenovo1996@gmail.com>
2025-10-22 10:59:38 +0700

    feat(sdk): update

--- lib/api/functional/comments/index.d.ts
+++ lib/api/functional/comments/index.d.ts
@@ -1,5 +1,5 @@
 import type { IConnection, Resolved, Primitive } from "@nestia/fetcher";
-import type { IResponse } from "../../../libs/shared/src/types/common.type";
+import type { IResponse, IQuery, IResponsePagination } from "../../../libs/shared/src/types/common.type";
 import type { IComment } from "../../../libs/shared/src/types/icomment";
 export declare function create(connection: IConnection, data: create.Input): Promise<create.Output>;
 export declare namespace create {
@@ -19,4 +19,20 @@ export declare namespace create {
         readonly status: 201;
     };
     const path: () => string;
+}
+export declare function findAll(connection: IConnection, query: findAll.Query): Promise<findAll.Output>;
+export declare namespace findAll {
+    type Query = Resolved<IQuery>;
+    type Output = Primitive<IResponse<IResponsePagination<IComment.ICommentResponse>>>;
+    const METADATA: {
+        readonly method: "GET";
+        readonly path: "/comments";
+        readonly request: null;
+        readonly response: {
+            readonly type: "application/json";
+            readonly encrypted: false;
+        };
+        readonly status: 200;
+    };
+    const path: (query: findAll.Query) => string;
 }
