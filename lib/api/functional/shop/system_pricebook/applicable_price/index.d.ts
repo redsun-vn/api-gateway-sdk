12959159760a39ad6fff55b04750f7f534e58a8e
avlunvu <ibmlenovo1996@gmail.com>
2026-02-03 16:12:40 +0700

    feat(sdk): update

---
+++ lib/api/functional/shop/system_pricebook/applicable_price/index.d.ts
@@ -0,0 +1,22 @@
+import type { IConnection, Resolved, Primitive } from "@nestia/fetcher";
+import type { IResponse } from "../../../../../libs/shared/src/types/common.type";
+import type { ISystemPricebook } from "../../../../../libs/shared/src/types/shop-service/system-pricebook.type";
+export declare function getApplicablePrice(connection: IConnection, productId: number, data: getApplicablePrice.Input): Promise<getApplicablePrice.Output>;
+export declare namespace getApplicablePrice {
+    type Input = Resolved<Omit<ISystemPricebook.IApplicablePricebookRequest, "productId">>;
+    type Output = Primitive<IResponse<ISystemPricebook.IApplicablePricebookResponse>>;
+    const METADATA: {
+        readonly method: "POST";
+        readonly path: "/shop/system-pricebook/:productId/applicable-price";
+        readonly request: {
+            readonly type: "application/json";
+            readonly encrypted: false;
+        };
+        readonly response: {
+            readonly type: "application/json";
+            readonly encrypted: false;
+        };
+        readonly status: 201;
+    };
+    const path: (productId: number) => string;
+}
