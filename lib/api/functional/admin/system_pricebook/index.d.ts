11dbc7967d6e645ce9ee0cfddf8aea51043267be
avlunvu <ibmlenovo1996@gmail.com>
2026-02-11 16:58:05 +0700

    feat(sdk): update

--- lib/api/functional/admin/system_pricebook/index.d.ts
+++ lib/api/functional/admin/system_pricebook/index.d.ts
@@ -72,3 +72,18 @@ export declare namespace update {
     };
     const path: (id: string) => string;
 }
+export declare function $delete(connection: IConnection, id: string): Promise<$delete.Output>;
+export declare namespace $delete {
+    type Output = Primitive<IResponse<false | true>>;
+    const METADATA: {
+        readonly method: "DELETE";
+        readonly path: "/admin/system-pricebook/:id";
+        readonly request: null;
+        readonly response: {
+            readonly type: "application/json";
+            readonly encrypted: false;
+        };
+        readonly status: 200;
+    };
+    const path: (id: string) => string;
+}
