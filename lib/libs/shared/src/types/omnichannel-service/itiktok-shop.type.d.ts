fa58da0911724e047431c0dc671f98a051527573
avlunvu <ibmlenovo1996@gmail.com>
2026-03-27 17:44:34 +0700

    feat(sdk): update

--- lib/libs/shared/src/types/omnichannel-service/itiktok-shop.type.d.ts
+++ lib/libs/shared/src/types/omnichannel-service/itiktok-shop.type.d.ts
@@ -1,9 +1,7 @@
 export declare namespace ITiktokShop {
     interface ICallback {
         code: string;
-        app_key: string;
         locale?: string;
         shop_region?: string;
-        state: string;
     }
 }
