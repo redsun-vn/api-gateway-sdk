cdba305b441ab6ae233c18e892410c185e357dca
DuongAnDev <duongandev@gmail.com>
2026-05-07 09:49:10 +0700

    feat(sdk): update

--- lib/api/functional/attachments/shop/index.d.ts
+++ lib/api/functional/attachments/shop/index.d.ts
@@ -2,3 +2,4 @@ export * as files from "./files";
 export * as spaces from "./spaces";
 export * as spaces_by_shop_id from "./spaces_by_shop_id";
 export * as issue_share_url from "./issue_share_url";
+export * as clinic_files from "./clinic_files";
