bea3e82c64779320c576cbfce3a9b805c322db65
avlunvu <ibmlenovo1996@gmail.com>
2026-08-28 12:25:55 +0700

    feat(sdk): update

--- lib/libs/shared/src/types/hr/salary/leave-accrual.d.ts
+++ lib/libs/shared/src/types/hr/salary/leave-accrual.d.ts
@@ -8,6 +8,7 @@ export declare namespace ILeaveAccrual {
         period_year: number;
         processed: number;
         granted: number;
+        expired: number;
         next_cursor: number | null;
         skipped: Record<string, number>;
     }
